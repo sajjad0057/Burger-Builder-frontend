085736aa3506019743990567930944dcfe8551da
sajjad Hossain <sajjadhossain0057@gamil.com>
2020-12-15 02:02:43 +0600

    Creating Checkout Option with normal way

--- src/Components/Orders/Checkout/Checkout.jsx
+++ src/Components/Orders/Checkout/Checkout.jsx
@@ -2,38 +2,37 @@ import React, { Component } from "react";
 import { Button } from "reactstrap";
 
 class Checkout extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            values: {
-              deleveryAddress: "",
-              phone: null,
-              paymentType: "Cash On Delivery",
-            },
-            
-          };
-          this.inputChangeHandenler=this.inputChangeHandenler.bind(this)
-          this.submitHandler = this.submitHandler.bind(this)
-          
-    }
+  state = {
+    values: {
+      deleveryAddress: "",
+      phone: "",
+      paymentType: "Cash On Delivery",
+    },
+  };
 
-  goBack=()=>{
-       this.props.history.goBack("/")
-  }
+  goBack = () => {
+    this.props.history.goBack("/");
+  };
 
-  inputChangeHandenler=(event)=>{
-      this.setState({
-          values : {
-              ...this.state.values,
-              [event.target.name]:event.target.value
-          }
-      })
+  inputChangeHandenler = (event) => {
+    this.setState({
+      values: {
+        ...this.state.values,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+  submitHandler = () => {
+    console.log("CheckOut --->", this.state.values);
+    this.setState({
+        values:{
+            deleveryAddress: "",
+            phone: "",
+            paymentType: "Cash On Delivery",
+        }
+    })
 
-  }
-  submitHandler=()=>{
-      console.log("CheckOut --->",this.state.values);
-      console.log("CheckOut --- this>",this);
-  }
+  };
 
   render() {
     return (
@@ -44,7 +43,7 @@ class Checkout extends Component {
             value={this.state.values.deleveryAddress}
             className="form-control"
             placeholder="Delivery Address"
-            onChange={this.inputChangeHandenler}
+            onChange={(e)=>this.inputChangeHandenler(e)}
           />
           <br />
           <input
@@ -52,24 +51,32 @@ class Checkout extends Component {
             value={this.state.values.phone}
             className="form-control"
             placeholder="Contact Number"
-            onChange={this.inputChangeHandenler}
+            onChange={(e)=>this.inputChangeHandenler(e)}
           />
           <br />
           <select
             name="paymentType"
             value={this.state.values.paymentType}
             className="form-control"
-            onChange={this.inputChangeHandenler}
+            onChange={(e)=>this.inputChangeHandenler(e)}
           >
             <option value="Cash On Delivery">Cash On Delivery</option>
             <option value="Bkash">Bkash</option>
           </select>
           <br />
-          <Button style={{ backgroundColor: "#f55195" }} className="mr-auto" onClick={this.submitHandler}>
+          <Button
+            style={{ backgroundColor: "#f55195" }}
+            className="mr-auto"
+            onClick={this.submitHandler}
+          >
             {" "}
             Place Order
           </Button>
-          <Button style={{ backgroundColor: "orange" }} className="ml-3" onClick={this.goBack}>
+          <Button
+            style={{ backgroundColor: "orange" }}
+            className="ml-3"
+            onClick={this.goBack}
+          >
             Cancel
           </Button>
         </form>
