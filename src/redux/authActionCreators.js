4cedf875bd5bce3c7c29fcb46779c2765e5f08b9
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-01-16 06:43:27 +0600

    Logging in user while signup with DRF

--- src/redux/authActionCreators.js
+++ src/redux/authActionCreators.js
@@ -26,6 +26,17 @@ export const authFailed = (errMsg) => {
   };
 };
 
+export const storeAuthDataLocally = (token) => {
+  const decoded_token = jwt_decode(token);
+  const expTime = decoded_token.exp;
+  const userId = decoded_token.user_id;
+  localStorage.setItem("token", token);
+  localStorage.setItem("userId", userId);
+  const expirationTime = new Date(expTime * 1000);
+  localStorage.setItem("expirationTime", expirationTime);
+  return userId;
+};
+
 export const auth = (email, password, signUpMode) => (dispatch) => {
   dispatch(authLoading(true));
   const authData = {
@@ -47,26 +58,30 @@ export const auth = (email, password, signUpMode) => (dispatch) => {
   }
 
   axios
-    .post(authUrl, authData, header)
+    .post(authUrl , authData , header)
     .then((response) => {
-      console.log("DRF response ----> : ", jwt_decode(response.data.access));
+      
       dispatch(authLoading(false));
       if (!signUpMode) {
         const token = response.data.access;
-        const decoded_token = jwt_decode(token);
-        const expTime = decoded_token.exp;
-        const userId = decoded_token.user_id;
-        localStorage.setItem("token", token);
-        localStorage.setItem("userId", userId);
-        const expirationTime = new Date(expTime * 1000);
-        localStorage.setItem("expirationTime", expirationTime);
+        const userId = storeAuthDataLocally(token);
         dispatch(authSuccess(token, userId));
-      }
+      } else {
+        dispatch(authLoading(true));
+        return axios.post("http://127.0.0.1:8000/api/token/", authData, header)
+            .then(response => {
+                dispatch(authLoading(false));
+                const token = response.data.access;
+                const user_id = storeAuthDataLocally(token);
+                dispatch(authSuccess(token, user_id));
+            })
+    }
+    console.log("DRF response ----> : ", jwt_decode(response.data.access));
     })
     .catch((err) => {
       // The Object.keys() method returns an array of a given object's own enumerable property
       const key = Object.keys(err.response.data)[0]; // DRF passing error messages as an object. so here we Useing js "Object" key to accessing required data.
-      console.log("err.response", err.response);
+      console.log("err.response", err.response.data[key]);
       dispatch(authFailed(`${key.toUpperCase()} : ${err.response.data[key]}`));
       dispatch(authLoading(false));
     });
